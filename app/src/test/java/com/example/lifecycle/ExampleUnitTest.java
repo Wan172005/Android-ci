c7213add2555de1c41f2b016c633e1c347658c60
Wan172005 <minhquang172005@gmail.com>
2026-01-26 15:17:15 +0700

    test ci: build failed

--- app/src/test/java/com/example/lifecycle/ExampleUnitTest.java
+++ app/src/test/java/com/example/lifecycle/ExampleUnitTest.java
@@ -24,7 +24,7 @@ public class ExampleUnitTest {
     @Test
     public void testGreenColor() {
 
-        int color = MainActivity.getColorFromName("huy");
+        int color = MainActivity.getColorFromName("thuan");
 
         assertEquals(0xff00ff00, color);
     }
